b8db79281e9198ea0b6cecb397fa9c10323de2c7
ali27-dev <khanali3229@gmail.com>
2026-01-22 22:20:23 +0500

    Applying styling with the help of css module ans also cretae some components.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./components/AppLayout";
 
 function App() {
   // const a = 23;
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
+import AppNav from "../components/AppNav";
 import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>HomePage</h1>
+      <Link to="/app">Go to the App</Link>
     </div>
   );
 }
