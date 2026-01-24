5f0e7a5848368e975ce874a2ea9cba3d4142bc44
ali27-dev <khanali3229@gmail.com>
2026-01-24 21:47:33 +0500

    Implementing the cities list,First of all i'm add fake data in json file and the use json server then i'm retrive that data from json server API and use it to display cities data.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,31 @@ import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./components/AppLayout";
+import { useEffect, useState } from "react";
+import CityList from "./components/CityList";
 // import City from "./components/City";
+const BASED_URL = `http://localhost:8000`;
 
 function App() {
-  // const a = 23;
+  const [cities, setIsCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASED_URL}/cities`);
+        const data = await res.json();
+        setIsCities(data);
+      } catch (error) {
+        alert("Something Went Wrong...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -17,8 +38,14 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>LIST</p>} />
-          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
