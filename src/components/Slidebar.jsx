e83356d8062020b47950ebff4fe2c838dbd76063
ali27-dev <khanali3229@gmail.com>
2026-01-24 11:18:30 +0500

    Nested Routes and Index Route.I'm implement in app Route to render the cities and countries.

--- src/components/Slidebar.jsx
+++ src/components/Slidebar.jsx
@@ -3,13 +3,15 @@ import Logo from "./Logo";
 import SidebarFooter from "./SlidebarFooter";
 import styles from "./Sidebar.module.css";
 import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
 
 function Slidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      <Outlet />
+      {/* <p>List of cities</p> */}
 
       <SidebarFooter />
     </div>
