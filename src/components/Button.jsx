c76de18ff7b18f6488d87b57856231f3f779ff74
ali27-dev <khanali3229@gmail.com>
2026-01-25 21:12:57 +0500

    Another learned new Hook useNavigate, and also use in form component.

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import styles from "./Button.module.css";
+function Button({ children, type, onClick }) {
+  return (
+    <button className={`${styles.btn} ${styles[type]}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
